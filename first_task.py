90767b6929187a4a9a6af720d7b92e53d8e24e48
khachkocode <serhii.m.khachko@ukd.edu.ua>
2024-02-14 10:43:38 +0200

    Edit: add line - task completed

--- first_task.py
+++ first_task.py
@@ -1,2 +1,3 @@
 print(f'Hi, my name is Khachko Serhii Mykolayovych and i\'m studing studying software development in college.')
 print(f'Hi, my name is Koiliak Maksym Ivanovych and i\'m studing studying software development in college.')
+print('Task completed')
