2038963f3ebcada854d8915f0d295eaeee344dd5
zukicode <zukicode@gmail.com>
2024-02-22 14:22:32 +0200

    del

--- first_task.py
+++
@@ -1,3 +0,0 @@
-print(f'Hi, my name is Khachko Serhii Mykolayovych and i\'m studing studying software development in college.')
-print(f'Hi, my name is Koiliak Maksym Ivanovych and i\'m studing studying software development in college.')
-print('Task completed')
